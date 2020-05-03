a3f7bfee375973932a84d607b7905e1455aa9907
MEDALI-APURIMAC <64325499+MEDALI-APURIMAC@users.noreply.github.com>
2020-05-02 19:20:43 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,198 @@
+<!doctype html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Turismo especializado en Huánuco.</title>
+	<link rel="stylesheet" href="estilos.css">
+  </head>
+  <body>
+  
+       <header id="1">
+	   
+	         <section id="logo">
+			   <a href="">
+			   <h2></h2>
+			   </a>
+			 
+			 </section>
+			 
+	         <section id="Inicio">
+			 
+			    <a href="#1">
+			   <h2>Inicio</h2>
+			   </a>
+			 
+			 </section>
+			 
+			 <section id="Nosotros">
+			 
+			    <a href="#2">
+			   <h2>Nosotros</h2>
+			   </a>
+			 
+			 </section>
+			 
+			 <section id="tours">
+			 
+			    <a href="">
+			   <h2>Tours Especializado</h2>
+			   </a>
+			 
+			 </section>
+			 
+			 <section id="Contacto">
+			 
+			    <a href="#3">
+			   <h2>Contacto</h2>
+			   </a>
+			 
+			 </section>
+			 
+			 <section id="Blog">
+			    <a href="">
+			   <h2>Blog</h2>
+			   </a>
+			 
+			 </section>
+	   
+	   
+	   
+	   
+	   </header>
+	   
+	   
+	   
+	   <nav>
+	        <section id="banner">
+			 
+			 </section>
+			 
+			 <section id="login">
+			 
+			 </section>
+			 
+			 <section id="banner">
+			 
+			 </section>
+	   
+	   
+	   
+	   
+	   
+	   
+	   </nav>
+	   
+	   
+	   
+	   
+	   <main id="2">
+	   
+	       <article>
+		   
+		   </article>
+		   
+		   <article>
+		   
+		   </article>
+		   
+		   <article>
+		   
+		   </article>
+	   
+	   
+	   
+	   
+	   
+	   </main>
+	   
+	   
+	   
+	   
+	   <footer id="3">
+	   
+	   
+	         <aside id="left">
+			 
+			 <center>
+			 <h2>SIGUENOS EN NUESTRAS REDES</h2>
+			 
+			 </center>
+			 
+			 
+			 
+			 <a target="_blank" href="https://www.facebook.com/profile.php?id=100022069533443">
+         <div class="circulo">
+         <img src="https://seduzacregion07.files.wordpress.com/2014/05/logo-facebook1.png" width="80px" height="80px" class="imagen" />
+         </div>
+         </a>
+  
+          <a target="_blank" href="https://wa.me/51927311924?text=Hola%20necesito%20informacion">
+         <div class="circulo">
+           <img src="https://banner2.kisspng.com/20180330/qzw/kisspng-whatsapp-logo-computer-icons-messenger-5abe20d1e68177.1133491015224096819442.jpg" width="80px" height="80px" class="imagen"  />
+            </div>
+          </a>
+           <a target="_blank" href="https://www.youtube.com/channel/UCpQnPLj2nWhGEVUF3_J_0mQ/">
+          <div class="circulo">
+           <img src="https://www.factmag.com/wp-content/uploads/2018/05/youtube-logo-081217-616x440.jpg" width="80px" height="80px" class="imagen" />
+            </div>
+         </a>
+  
+             <center>
+			 <h2>CONTACTO DE LA EMPRESA</h2>
+			 
+			 </center>
+			 <p>EMPRESA: RAYWA PERÚ SAC</p>
+			  <p>CORREO: raywaperu.com</p>
+			   <p>PROGRAMADOR: Geronimo,Rodrigo</p>
+			 
+			 </aside>
+			 
+
+			 
+			 <aside id="right">
+			 
+			     <form action="enviar2.php" method="post" >
+				    <center>
+				    <h2>CONTACTO</h2>
+					</center>
+					<input type="text" name="Nombre" required placeholder="Rodrigo Geronimo">
+					
+					<input type="text" name="correo" required placeholder="ejemplo@.com">
+					
+					<input type="text" name="telefono" required placeholder="51927311924">
+					
+					<input type="text" name="pais" required placeholder="Perú">
+					
+					<textarea name="Mensaje" required placeholder="Deja aqui su mensaje"></textarea>
+					
+					<input type="submit" value="Reservar" id="boton">
+					
+					
+				   
+				 
+				 </form>
+			 
+			 
+			 </aside>
+	   
+	   
+	   
+	   
+	   
+	   </footer>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  </body>
+  
+ </html>
